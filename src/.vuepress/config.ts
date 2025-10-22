500558d46d1450eaa81dfca443c775145258462e
南絮 <1275454303@qq.com>
2025-10-22 08:58:05 +0800

    初始化文档

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,18 @@
+import { defineUserConfig } from "vuepress";
+
+import theme from "./theme.js";
+
+export default defineUserConfig({
+  base: "/",
+
+  lang: "zh-CN",
+  title: "文档演示",
+  description: "vuepress-theme-hope 的文档演示",
+
+  theme,
+
+  port: 9000
+
+  // 和 PWA 一起启用
+  // shouldPrefetch: false,
+});
