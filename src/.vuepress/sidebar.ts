919c4529f4195a822dec8310fba89c4cefb7d579
Nanxu <1275454303@qq.com>
2026-03-12 15:10:54 +0800

    feat(.vuepress): 移除无用页面

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -3,31 +3,12 @@ import { sidebar } from "vuepress-theme-hope";
 export default sidebar({
   "/": [
     "",
-    "portfolio",
     {
       text: "Nanxu",
       icon: "book",
       prefix: "nanxu/",
       link: "nanxu/",
       children: "structure"
-    },
-    {
-      text: "案例",
-      icon: "laptop-code",
-      prefix: "demo/",
-      link: "demo/",
-      children: "structure",
-    },
-    {
-      text: "文档",
-      icon: "book",
-      prefix: "guide/",
-      children: "structure",
-    },
-    {
-      text: "幻灯片",
-      icon: "person-chalkboard",
-      link: "https://ecosystem.vuejs.press/zh/plugins/markdown/revealjs/demo.html",
-    },
+    }
   ],
 });
